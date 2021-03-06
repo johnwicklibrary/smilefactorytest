bb119a286200c3625a76da0d42a19214ad6fb8c9
user <user@liverpools-MacBook-Pro.local>
2021-03-06 09:33:19 -0800

    test smile factory json

---
+++ src/main/java/com/example/Smile/Factory/demo/SmileFactoryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Smile.Factory.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileFactoryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileFactoryDemoApplication.class, args);
+	}
+
+}
